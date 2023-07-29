4e7d9a914ae1d302f3b0bdc573891bfbcfc9614b
Tim King <kingtimm@gmail.com>
2023-07-28 19:29:02 -0500

    removing underlines in prose

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,3 +12,18 @@
 <script setup lang="ts">
 
 </script>
+
+<style>
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 a {
+    text-decoration: none;
+}
+
+.prose :where(a):not(:where(.not-prose, .not-prose *, h1, h2, h3, h4, h5, h6)) {
+    text-decoration: none;
+}
+</style>
