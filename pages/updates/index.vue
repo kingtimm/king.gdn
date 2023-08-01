ef67f6a61e35f84e4b9707ce5c169e2cd65b57d2
Tim King <kingtimm@gmail.com>
2023-07-31 21:36:41 -0500

    new article - development as a hobby

--- pages/updates/index.vue
+++ pages/updates/index.vue
@@ -6,17 +6,15 @@
         <div class="w-full">
             <ul class="ml-5 list-disc">
                 <ContentNavigation v-slot="{ navigation }" :query="projectsQuery">
-                    <li>
-                        <div v-for="link of navigation[0].children" :key="link._path" class="w-full">
-                            <NuxtLink :to="link._path">
-                                <span class="font-bold">
-                                    {{ link.title }} -
-                                </span>
-                                <span class="font-normal">
-                                    <NuxtTime :datetime="link.publishedAt" day="numeric" month="long" year="numeric" />
-                                </span>
-                            </NuxtLink>
-                        </div>
+                    <li v-for="link of navigation[0].children.reverse()" :key="link._path" class="w-full">
+                        <NuxtLink :to="link._path">
+                            <span class="font-bold">
+                                {{ link.title }} -
+                            </span>
+                            <span class="font-normal">
+                                <NuxtTime :datetime="link.publishedAt" day="numeric" month="long" year="numeric" />
+                            </span>
+                        </NuxtLink>
                     </li>
                 </ContentNavigation>
             </ul>
