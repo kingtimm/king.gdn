1d720e598beb8ede080668eeace13959ffc5b179
Tim King <kingtimm@gmail.com>
2023-08-06 13:36:51 -0500

    removing element selectors too

--- components/OgImage/Default.vue
+++ components/OgImage/Default.vue
@@ -24,17 +24,16 @@ const shouldShowImage = computed(() => {
 <template>
     <div style="color: #1c1c1c; background-image: linear-gradient(-135deg, #cccccc 0%, #399FB1 100%);"
         class="pl-8 w-full h-full flex flex-col justify-center">
-        <img v-if="shouldShowImage" :src="props.posterImage"
-            class="h-48 w-96 posterimage left-0 top-0 object-cover object-top" />
-        <h1>
+        <img v-if="shouldShowImage" :src="props.posterImage" class="h-48 w-96 left-0 top-0 object-cover object-top" />
+        <h1 style="font-size: 70px;">
             {{ props.title }}
         </h1>
-        <h2>
+        <h2 style="font-size: 40px; font-weight: normal;">
             {{ props.description }}
         </h2>
     </div>
-    <div style="font-size: 50px" class="absolute right-0 top-0 m-8 logo text-right">
-        <span style="color: #399FB1; font-weight: bold;" class="font-bold king">king</span>
+    <div style="font-size: 50px" class="absolute right-0 top-0 m-8 text-right">
+        <span style="color: #399FB1; font-weight: bold;">king</span>
         <span>.gdn</span>
         <p>
             <NuxtTime class="text-right text-xl opacity-80" :datetime="props.publishedAt!" day="numeric" month="long"
@@ -43,14 +42,3 @@ const shouldShowImage = computed(() => {
         </p>
     </div>
 </template>
-
-<style scoped>
-h1 {
-    font-size: 70px;
-}
-
-h2 {
-    font-size: 40px;
-    font-weight: normal;
-}
-</style>
