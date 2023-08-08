4d689171173ad82a9e0079b92ff0bf12969a0ac8
Tim King <kingtimm@gmail.com>
2023-08-07 21:15:16 -0500

    redesigned ogimages

--- components/OgImage/Default.vue
+++ components/OgImage/Default.vue
@@ -22,23 +22,28 @@ const shouldShowImage = computed(() => {
 </script>
 
 <template>
-    <div style="color: #1c1c1c; background-image: linear-gradient(-135deg, #cccccc 0%, #399FB1 100%);"
-        class="pl-8 w-full h-full flex flex-col justify-center">
-        <img v-if="shouldShowImage" :src="props.posterImage" class="h-48 w-96 left-0 top-0 object-cover object-top" />
-        <h1 style="font-size: 70px;">
-            {{ props.title }}
-        </h1>
-        <h2 style="font-size: 40px; font-weight: normal;">
-            {{ props.description }}
-        </h2>
-    </div>
-    <div style="font-size: 50px" class="absolute right-0 top-0 m-8 text-right">
-        <span style="color: #399FB1; font-weight: bold;">king</span>
-        <span>.gdn</span>
-        <p>
-            <NuxtTime class="text-right text-xl opacity-80" :datetime="props.publishedAt!" day="numeric" month="long"
-                year="numeric">
-            </NuxtTime>
-        </p>
+    <div style="color: #1c1c1c; background-image: linear-gradient(135deg, #cccccc 0%, #399FB1 100%);"
+        class="w-full h-full flex">
+        <div class="flex flex-col h-full p-8" :class="shouldShowImage ? 'w-2/3' : 'w-full'">
+            <div style="font-size: 60px" class="flex-none">
+                <span style="color: #399FB1; font-weight: bold;">king</span>.gdn
+                <p class="-mt-6">
+                    <NuxtTime class="text-right text-2xl opacity-80" :datetime="props.publishedAt!" day="numeric"
+                        month="long" year="numeric">
+                    </NuxtTime>
+                </p>
+            </div>
+            <div class="grow flex flex-col justify-center">
+                <h1 class="my-1" style="font-size: 70px;">
+                    {{ props.title }}
+                </h1>
+                <h2 style="font-size: 40px; font-weight: normal;">
+                    {{ props.description }}
+                </h2>
+            </div>
+        </div>
+        <div class="">
+            <img v-if="shouldShowImage" :src="props.posterImage" class="h-full w-96 left-0 top-0 object-cover" />
+        </div>
     </div>
 </template>
