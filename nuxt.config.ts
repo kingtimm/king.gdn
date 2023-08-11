32a5eba2b9c9e156731a306d9e3a8bec4e93176b
Tim King <kingtimm@gmail.com>
2023-08-11 07:42:37 -0500

    updating viewport for ios

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,14 @@ export default defineNuxtConfig({
     'nuxt-og-image'
   ],
   css: ['@unocss/reset/tailwind.css'],
+  app: {
+    head: {
+      viewport: 'width=device-width,initial-scale=1,viewport-fit=cover',
+      meta: [
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+      ]
+    }
+  },
   experimental: {
     viewTransition: true
   },
